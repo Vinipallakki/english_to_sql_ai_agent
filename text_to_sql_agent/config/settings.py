e37e2370ca92ee24756fdf7738b97211565e01ba
pallakki <98947934+Vinipallakki@users.noreply.github.com>
2025-09-27 13:49:44 +0530

    Update settings.py

--- text_to_sql_agent/config/settings.py
+++ text_to_sql_agent/config/settings.py
@@ -1,4 +1,4 @@
-PROJECT_ID = "gen-lang-client-0236559629"
-DATASET_ID = "demo_ds"
-MODEL_NAME = "gemini-2.5-flash"   # or try "gemini-1.5-pro-latest"
+PROJECT_ID = "your project id"
+DATASET_ID = "your dataset id"
+MODEL_NAME = "pick any model"   # or try "gemini-1.5-pro-latest"
 GEMINI_API_KEY = "keep your api key here"
